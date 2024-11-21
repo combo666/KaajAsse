d18de5cb59934a5c255b073b4805cb6feb125efe
sshopnil1 <shopnil710@gmail.com>
2024-11-22 00:52:48 +0600

    db connection and session

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+    if(isset($_SESSION['uname'])){
+        $uname = $_SESSION['uname'];
+        // echo "jidisjd";
+    }
+    
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +16,14 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php include('conf/database/db_connect.php'); ?>
+
+    <?php
+        if(!isset($_SESSION['uname'])){
+            echo $_SESSION['uname'];
+            ?>
+    <?php
+        }
+    ?>
 </body>
 </html>
