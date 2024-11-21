16eaa05ef2601745712e702a82a31551f2b7b7b8
sshopnil1 <shopnil710@gmail.com>
2024-11-22 02:14:11 +0600

    server connection fix

--- conf/database/db_connect.php
+++ conf/database/db_connect.php
@@ -1,15 +1,15 @@
 <?php
-    $DB['db_host'] = "sql3.freesqldatabase.com";
-    $DB['db_user'] = "sql3746436";
-    $DB['db_pass'] = "nmE9qv5ZjG";
-    $DB['db_name'] = "sql3746436";
+    // $DB['db_host'] = "sql3.freesqldatabase.com";
+    // $DB['db_user'] = "sql3746436";
+    // $DB['db_pass'] = "nmE9qv5ZjG";
+    // $DB['db_name'] = "sql3746436";
 
-    foreach($DB as $key=>$value)
-    {
-        define(strtoupper($key), $value);
-    }
+    // foreach($DB as $key=>$value)
+    // {
+    //     define(strtoupper($key), $value);
+    // }
     
-    $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $connect = mysqli_connect("sql3.freesqldatabase.com", "sql3746436", "nmE9qv5ZjG", "sql3746436");
 
     if(!$connect)
     {
