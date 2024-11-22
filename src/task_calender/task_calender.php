a6cf69db8c94708e76c9ef97ff02b97ec0824437
combo666 <shadat1124@gmail.com>
2024-11-23 01:03:05 +0600

    static calendar added

--- src/task_calender/task_calender.php
+++ src/task_calender/task_calender.php
@@ -1,6 +1,16 @@
 
 <?php include('../../examples/includes/header.php'); ?>
 
+<?php
+include 'Calendar.php';
+$calendar = new Calendar('2024-05-12');
+$calendar->add_event('Birthday', '2024-05-03', 1, 'green');
+$calendar->add_event('Doctors', '2024-05-04', 1, 'red');
+$calendar->add_event('Holiday', '2024-05-16', 7);
+?>
+
+
+<?=$calendar?>
 
 <?php include('../../examples/includes/navbar.php'); ?>
 
