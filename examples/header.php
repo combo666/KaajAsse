cf3f0ed975ad557ecf0eef0cc10ac1a139f02bf3
combo666 <shadat1124@gmail.com>
2024-11-23 00:43:56 +0600

    static calendar added

--- examples/header.php
+++ examples/header.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KaajAsse</title>
+<<<<<<< Updated upstream:examples/header.php
     <link rel="icon" href="../assets/img/icon.ico">
     <link rel="stylesheet" href="../assets/css/style.css">
+=======
+    <link rel="icon" href="../../assets/img/icon.ico">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/calendar.css">
+>>>>>>> Stashed changes:examples/includes/header.php
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
