d7c5438b881b84a65b9a4933ed0c4a9c1e8be09d
combo666 <shadat1124@gmail.com>
2024-11-24 01:56:47 +0600

    static running day task container created

--- src/task_calender/task_calender.php
+++ src/task_calender/task_calender.php
@@ -1,6 +1,7 @@
 
 <?php include('../../examples/includes/header.php'); ?>
 
+
 <?php
 include 'Calendar.php';
 $calendar = new Calendar('2024-05-12');
@@ -9,8 +10,14 @@ $calendar->add_event('Doctors', '2024-05-04', 1, 'red');
 $calendar->add_event('Holiday', '2024-05-16', 7);
 ?>
 
+<div class="parent">
+<div class="div1"> <?=$calendar?> </div>
+<div class="div2"> <?php include('todays_task.php')  ?> </div>
+<div class="div3"> </div>
+</div>
 
-<?=$calendar?>
+
+
 
 <?php include('../../examples/includes/navbar.php'); ?>
 
---
+++ src/task_calender/todays_task.php
@@ -0,0 +1,27 @@
+<div class="task_container">
+    <div class="task_header">
+        Today's Task
+    </div>
+    <div class="tasks_bg">
+        <div class="task_card">
+            <div style="display: flex;
+            align-items: center;
+            padding: 0px;">
+                <span class="dot_priority"></span>
+                <p class="date">Date</p>
+            </div>
+            <p class="task_name">Task X</p>
+            <p class="task_note">Your notes</p>
+        </div>
+        <div class="task_card">
+        <div style="display: flex;
+            align-items: center;
+            padding: 0px;">
+                <span class="dot_priority"></span>
+                <p class="date">Date</p>
+            </div>
+            <p class="task_name">Task X</p>
+            <p class="task_note">Your notes</p>
+        </div>
+    </div>
+</div>
