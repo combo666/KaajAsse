7f7e8d06d915e71a42efcc05e43dee4010c877cb
Shayanta Shopnil <shopnil710@gmail.com>
2024-12-21 00:11:28 +0600

    registration backend

--- src/login/login_reg.php
+++ src/login/login_reg.php
@@ -1,37 +1,55 @@
-
 <?php
 include '../../conf/database/db_connect.php';
-    if(isset($_POST['sign-in'])){
-        $email = mysqli_real_escape_string($connect, $_POST['email']);
-        $password = mysqli_real_escape_string($connect, $_POST['password']);
+if (isset($_POST['sign-in'])) {
+    $email = mysqli_real_escape_string($connect, $_POST['email']);
+    $password = md5($_POST['password']);
 
-        $select = " SELECT * FROM KaajAsse.user WHERE user_email = '$email';";
-        $result = mysqli_query($connect, $select);
-        // echo $result;
-        if(mysqli_num_rows($result) > 0) {
-            $user = mysqli_fetch_assoc($result); 
-            if ($password == $user['user_pass']) {
-                // session_start();
-                // $_SESSION['user_id'] = $user['user_id']; 
-                // $_SESSION['user_email'] = $user['user_email'];
-                
-                // Redirect to a welcome or dashboard page
-                // header("Location: welcome.php");
-                echo "good to see ya";
-                // exit();
+    $select = " SELECT * FROM KaajAsse.user WHERE user_email = '$email';";
+    $result = mysqli_query($connect, $select);
+    // echo $result;
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+        if ($password == $user['user_pass']) {
+            session_start();
+            $_SESSION['user_id'] = $user['user_id']; 
+            $_SESSION['user_email'] = $user['user_email'];
 
-
-            } else {
-                echo "Wrong password!";
-            }
+            header("Location: ../projects/index.php");
+            exit();
 
         } else {
-            echo "User not found!";
+            echo "Wrong password!";
+        }
+
+    } else {
+        echo "User not found!";
+    }
+} else if (isset($_POST["sign-up"])) {
+    $fname = mysqli_real_escape_string($connect, $_POST['fname']);
+    $lname = mysqli_real_escape_string($connect, $_POST['lname']);
+    $email = mysqli_real_escape_string($connect, $_POST['email']);
+    $pass = md5($_POST['password']);
+    $cpass = md5($_POST['cpassword']);
+
+    $select = " SELECT * FROM KaajAsse.user WHERE user_email = '$email' && user_pass = '$pass' ";
+
+    $result = mysqli_query($connect, $select);
+
+    if (mysqli_num_rows($result) > 0) {
+
+        $error[] = 'user already exist!';
+
+    } else {
+
+        if ($pass != $cpass) {
+            $error[] = 'passwords did not match!';
+        } else {
+            $insert = "INSERT INTO KaajAsse.user(first_name, last_name, user_email, user_pass) VALUES('$fname','$lname','$email','$pass')";
+            mysqli_query($connect, $insert);
+            echo "Now Login";
         }
     }
-    else if(isset($_POST["sign-up"])){
-        
-    }
+}
 
 ?>
 
@@ -78,7 +96,10 @@ include '../../conf/database/db_connect.php';
 
             <form id="signup-form" method="POST" style="display: none;">
                 <div class="form-group">
-                    <input type="text" name="fullname" placeholder="Full Name" required>
+                    <input type="text" name="fname" placeholder="First Name" required>
+                </div>
+                <div class="form-group">
+                <input type="text" name="lname" placeholder="Last Name">
                 </div>
                 <div class="form-group">
                     <input type="email" name="email" placeholder="Email" required>
@@ -87,10 +108,7 @@ include '../../conf/database/db_connect.php';
                     <input type="password" name="password" placeholder="Password" required>
                 </div>
                 <div class="form-group">
-                    <label>
-                        <input type="checkbox" name="terms" required>
-                        I accept the <a href="#">Terms and conditions</a>
-                    </label>
+                    <input type="password" name="cpassword" placeholder="Confirm Password" required>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="signin-btn" name="sign-up">SIGN UP</button>
